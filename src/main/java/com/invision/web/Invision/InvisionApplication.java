d8415f6438d26eda228d7d3d81671564ba5a167d
AC_Baleni <baleniicaviwe@gmail.com>
2026-06-03 14:07:42 +0200

    Initial commit

---
+++ src/main/java/com/invision/web/Invision/InvisionApplication.java
@@ -0,0 +1,13 @@
+package com.invision.web.Invision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/invision/web/Invision/InvisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invision.web.Invision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
