974970eaa31471d005f402bcb93e3944e5637288
AC_Baleni <baleniicaviwe@gmail.com>
2026-06-03 15:30:06 +0200

    add: Role enum and update user model

---
+++ src/main/java/com/invision/web/Invision/model/Role.java
@@ -0,0 +1,6 @@
+package com.invision.web.Invision.model;
+
+public enum Role {
+
+    ADMIN, MANAGER, BORROWER
+}
--- src/main/java/com/invision/web/Invision/model/User.java
+++ src/main/java/com/invision/web/Invision/model/User.java
@@ -1,4 +1,20 @@
 package com.invision.web.Invision.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long privateId;
+    private String name;
+    private String department;
+    private String email;
+    private String password;
+    private Role role;
+
 }
